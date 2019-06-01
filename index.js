cc4018cf1ac8ca18670e28bbf3e6e8d281c45b30
rsclarke <rsclrk@pm.me>
2019-06-01 08:36:08 +0800

    lint and format

--- index.js
+++ index.js
@@ -1,112 +1,111 @@
 const shiki = require('shiki')
 const visit = require('unist-util-visit')
-const { commonLangIds, commonLangAliases, otherLangIds } = require('shiki-languages')
+const {
+  commonLangIds,
+  commonLangAliases,
+  otherLangIds
+} = require('shiki-languages')
 const hastToString = require('hast-util-to-string')
 const u = require('unist-builder')
 
-const languages = [
-    ...commonLangIds,
-    ...commonLangAliases,
-    ...otherLangIds
-]
+const languages = [...commonLangIds, ...commonLangAliases, ...otherLangIds]
 
 module.exports = attacher
 
 function attacher(options) {
-    var settings = options || {}
-    var theme = settings.theme || 'nord'
-    var shikiTheme
-    var highlighter
+  var settings = options || {}
+  var theme = settings.theme || 'nord'
+  var shikiTheme
+  var highlighter
 
-    try {
-        shikiTheme = shiki.getTheme(theme)
+  try {
+    shikiTheme = shiki.getTheme(theme)
+  } catch (error) {
+    shikiTheme = shiki.loadTheme(theme)
+  }
 
+  return transformer
+
+  async function transformer(tree) {
+    highlighter = await shiki.getHighlighter({
+      theme: shikiTheme,
+      langs: languages
+    })
+    visit(tree, 'element', visitor)
+  }
+
+  function visitor(node, index, parent) {
+    if (!parent || parent.tagName !== 'pre' || node.tagName !== 'code') {
+      return
     }
-    catch (e){
-        shikiTheme = shiki.loadTheme(theme)
-    }
-    
-    return transformer
 
-    async function transformer(tree) {
-        highlighter = await shiki.getHighlighter({
-            theme: shikiTheme,
-            langs: languages
-        })
-        visit(tree, 'element', visitor)
+    addStyle(parent, 'background: ' + shikiTheme.bg)
+
+    const lang = codeLanguage(node)
+
+    if (!lang) {
+      // Unknown language, fall back to a foreground colour
+      addStyle(node, 'color: ' + shikiTheme.colors['terminal.foreground'])
+      return
     }
 
-    function visitor(node, index, parent) {
+    const tokens = highlighter.codeToThemedTokens(hastToString(node), lang)
+    const tree = tokensToHast(tokens)
 
-        if (!parent || parent.tagName !== 'pre' || node.tagName !== 'code'){
-            return
-        }
-
-        addStyle(parent, "background: " + shikiTheme.bg)
-
-        const lang = codeLanguage(node)
-
-        if (!lang){
-            // Unknown language, fall back to a foreground colour
-            addStyle(node, "color: " + shikiTheme.colors['terminal.foreground'])
-            return
-        }
-        
-        const tokens = highlighter.codeToThemedTokens(hastToString(node), lang)
-        const tree = tokensToHast(tokens)
-
-        node.children = tree      
-    }
+    node.children = tree
+  }
 }
 
 function tokensToHast(lines) {
-    let tree = []
+  let tree = []
 
-    for (const line of lines) {
-        if (line.length === 0){
-            tree.push(u('text', '\n'))
-        }
-        else {
-            for (const token of line){
-                tree.push(
-                    u('element', 
-                        {
-                            tagName: 'span', 
-                            properties: { style: "color: "+token.color}
-                        }, 
-                        [u('text', escapeHtml(token.content))]
-                    )
-                )
-            }
-            tree.push(u('text','\n'))
-        }
+  for (const line of lines) {
+    if (line.length === 0) {
+      tree.push(u('text', '\n'))
+    } else {
+      for (const token of line) {
+        tree.push(
+          u(
+            'element',
+            {
+              tagName: 'span',
+              properties: {style: 'color: ' + token.color}
+            },
+            [u('text', escapeHtml(token.content))]
+          )
+        )
+      }
+
+      tree.push(u('text', '\n'))
     }
-    return tree
+  }
+
+  return tree
 }
 
-function addStyle(node, style){
-    var props = node.properties || {}
-    var styles = props.style || []
-    styles.push(style)
-    props.style = styles
-    node.properties = props
+function addStyle(node, style) {
+  var props = node.properties || {}
+  var styles = props.style || []
+  styles.push(style)
+  props.style = styles
+  node.properties = props
 }
 
-function escapeHtml(html){
-    return html.replace(/</g, '&lt;').replace(/>/g, '&gt;')
+function escapeHtml(html) {
+  return html.replace(/</g, '&lt;').replace(/>/g, '&gt;')
 }
 
 function codeLanguage(node) {
-    const className = node.properties.className || []
-    var value
+  const className = node.properties.className || []
+  var value
 
-    for(var i = 0; i < className.length; i++) {
-        value = className[i]
+  for (var i = 0; i < className.length; i++) {
+    value = className[i]
 
-        if (value.slice(0, 9) === 'language-') {
-            return value.slice(9)
-        }
+    if (value.slice(0, 9) === 'language-') {
+      return value.slice(9)
     }
+  }
 
-    return null
+  return null
 }
